6ba2d54c95c2dc4f65d4a562403adaff119c88d0
AriaAkbariyeh <aria.ak@hotmail.com>
2019-07-28 23:29:25 +0800

    Add DataAccessLayer interface and a in memory class

---
+++ Vault.Data/IPasswordData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vault.Core;
+
+namespace Vault.Data
+{
+    public interface IPasswordData
+    {
+        IEnumerable<Password> GetAll();
+    }
+}
---
+++ Vault.Data/InMemoryPasswordData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vault.Core;
+using System.Linq;
+
+namespace Vault.Data
+{
+    class InMemoryPasswordData : IPasswordData
+    {
+        readonly List<Password> passwords;
+        public InMemoryPasswordData()
+        {
+            passwords = new List<Password>()
+            {
+                new Password(){ Id = 1, Name = "MongoDB Atlas", Passphrase="Aria1234" },
+                new Password(){ Id = 2, Name = "MongoDB Lab", Passphrase="Aria1234" },
+                new Password(){ Id = 3, Name = "Azure", Passphrase="Aria1234" },
+            };
+        }
+
+        public IEnumerable<Password> GetAll()
+        {
+            return from r in passwords
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
