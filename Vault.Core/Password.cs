ac3efdbff431034bc7e0d9075a2c46e9afa8f381
AriaAkbariyeh <aria.ak@hotmail.com>
2019-07-28 22:55:14 +0800

    Add List Page Add Valut.Core Projects for Model

---
+++ Vault.Core/Password.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vault.Core
+{
+    public class Password
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Passphrase { get; set; }
+    }
+}
