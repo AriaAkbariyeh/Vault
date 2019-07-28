41f2e2f46e3f75b62fa7e5777a3e3ef62714f584
AriaAkbariyeh <aria.ak@hotmail.com>
2019-07-28 23:37:41 +0800

    add passwordData to ListModel class

--- Vault/Pages/Vault/List.cshtml.cs
+++ Vault/Pages/Vault/List.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Vault.Core;
+using Vault.Data;
 
 namespace Vault.Pages.Vault
 {
     public class ListModel : PageModel
     {
+        private readonly IPasswordData passwordData;
+        public IEnumerable<Password> passwords { get; set; }
+
+        public ListModel(IPasswordData passwordData)
+        {
+            this.passwordData = passwordData;
+        }
         public void OnGet()
         {
-
+            passwords = passwordData.GetAll();
         }
     }
 }
