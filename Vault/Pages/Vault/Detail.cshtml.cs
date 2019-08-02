ab0a3b39a29a1f6ff00b5dc52c49a35e579336ee
AriaAkbariyeh <aria.ak@hotmail.com>
2019-08-02 17:03:45 +0800

    add detail page

--- Vault/Pages/Vault/Detail.cshtml.cs
+++ Vault/Pages/Vault/Detail.cshtml.cs
@@ -4,14 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Vault.Core;
 
 namespace Vault.Pages.Vault
 {
     public class DetailModel : PageModel
     {
+        public Password Password{ get; set; }
+
         public void OnGet()
         {
-
+            Password = new Password();
         }
     }
 }
