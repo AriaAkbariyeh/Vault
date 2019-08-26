b08df8eca50a03abedfe18d96ce507e9be0a2572
AriaAkbariyeh <aria.ak@hotmail.com>
2019-08-26 19:59:20 +0800

    Add Edit Page

--- Vault/Pages/Vault/Edit.cshtml.cs
+++ Vault/Pages/Vault/Edit.cshtml.cs
@@ -4,14 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Vault.Core;
+using Vault.Data;
 
 namespace Vault.Pages.Vault
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IPasswordData passwordData;
+
+        public Password Password { get; set; }
+
+        public EditModel(IPasswordData passwordData)
+        {
+            this.passwordData = passwordData;
+        }
+
+        public IActionResult OnGet(int passwordId)
         {
 
+            Password = passwordData.GetPasswordById(passwordId);
+
+            if (Password == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
         }
     }
 }
