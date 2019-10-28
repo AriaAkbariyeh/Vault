dafd7d576f64e7d9afd644e326c29421614a5b32
AriaAkbariyeh <aria.ak@hotmail.com>
2019-10-28 15:42:29 +0800

    Create EntityFrameworkDBContext

---
+++ Vault.Data/EntityFrameworkDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Vault.Core;
+
+namespace Vault.Data
+{
+    class EntityFrameworkDBContext : DbContext
+    {
+        public DbSet<Password> Password { get; set; }
+    }
+}
