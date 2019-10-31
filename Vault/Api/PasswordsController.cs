b7a9e4391e39f08edb4015efb05fce89d7267e87
AriaAkbariyeh <aria.ak@hotmail.com>
2019-10-31 18:20:21 +0800

    Add PasswordsContrller API

---
+++ Vault/Api/PasswordsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vault.Core;
+using Vault.Data;
+
+namespace Vault.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordsController : ControllerBase
+    {
+        private readonly EntityFrameworkDBContext _context;
+
+        public PasswordsController(EntityFrameworkDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Passwords
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Password>>> GetPassword()
+        {
+            return await _context.Password.ToListAsync();
+        }
+
+        // GET: api/Passwords/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Password>> GetPassword(int id)
+        {
+            var password = await _context.Password.FindAsync(id);
+
+            if (password == null)
+            {
+                return NotFound();
+            }
+
+            return password;
+        }
+
+        // PUT: api/Passwords/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPassword(int id, Password password)
+        {
+            if (id != password.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(password).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PasswordExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Passwords
+        [HttpPost]
+        public async Task<ActionResult<Password>> PostPassword(Password password)
+        {
+            _context.Password.Add(password);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPassword", new { id = password.Id }, password);
+        }
+
+        // DELETE: api/Passwords/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Password>> DeletePassword(int id)
+        {
+            var password = await _context.Password.FindAsync(id);
+            if (password == null)
+            {
+                return NotFound();
+            }
+
+            _context.Password.Remove(password);
+            await _context.SaveChangesAsync();
+
+            return password;
+        }
+
+        private bool PasswordExists(int id)
+        {
+            return _context.Password.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Vault/Pages/Vault/ClientPasswords.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vault.Pages.Vault
+{
+    public class ClientPasswordsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
